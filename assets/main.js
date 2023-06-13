7992cc4ebf87b9e36e4a6f621559f0724c4b1ed7
Nicholas Gregory <nicholas.babcock092@gmail.com>
2023-06-12 20:35:32 -0700

    Added comments and made clarity changes

--- assets/main.js
+++ assets/main.js
@@ -3,6 +3,7 @@ var newsResults = document.getElementById("news-results");
 function makeNewsApiRequest(searchOptions) {    
     var url = "https://newsdata.io/api/1/news?apikey=pub_2427444b5c3582055dfc0d123514e20901632";
     
+    // Modify query string to include search parameters
     if (searchOptions.q) {
         url += ("&q=" + searchOptions.q);
     }
@@ -13,6 +14,7 @@ function makeNewsApiRequest(searchOptions) {
         url += ("&language=" + searchOptions.language);
     }
 
+    // Fetch data, then display results
     url = encodeURI(url);
     fetch(url).then(function(response) {
         return response.json();
@@ -34,6 +36,7 @@ function handleSearchInput(event) {
     var countryInput = searchCountry.value;
     var languageInput = searchLanguage.value
 
+    // Build options object to pass to makeNewsApiRequest from user input
     var optionObj = {};
     optionObj.q = keywordInput;
     optionObj.country = countryInput;
@@ -57,19 +60,26 @@ function saveArticle(headline, link, description) {
 }
 
 function makeResultsLi(headline, link, description) {
+    // Build HTML elements to display
     var resultLi = document.createElement("li");
     resultLi.setAttribute("class", "results-container");
+
     var headlineA = document.createElement("a");
     headlineA.setAttribute("href", link);
     headlineA.textContent = headline;
+
     var descriptionP = document.createElement("p");
     descriptionP.textContent = description;
+
     var icon = document.createElement("i");
     icon.setAttribute("class", "fa-sharp fa-regular fa-star");
+
+    //Add individual event listeners
     icon.addEventListener("click", () => {
         saveArticle(headline, link, description);
     })
 
+    // Append to root element
     resultLi.append(headlineA);
     resultLi.append(descriptionP);
     resultLi.append(icon);
